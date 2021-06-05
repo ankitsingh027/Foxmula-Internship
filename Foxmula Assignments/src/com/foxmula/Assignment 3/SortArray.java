d872e9e252792c28445e91ae90d73717f67472ad
Ankit Singh <74855474+ankitsingh027@users.noreply.github.com>
2021-06-05 11:10:39 +0530

    Update SortArray.java

--- Foxmula Assignments/src/com/foxmula/Assignment 3/SortArray.java
+++ Foxmula Assignments/src/com/foxmula/Assignment 3/SortArray.java
@@ -1,62 +1,41 @@
 package com.foxmula.assignment3;
 
+// Java program to count frequencies of array items
 import java.util.*;
-import java.util.Map.Entry;
-
-public class SortFrequencyArray {
-	
-	 void frequencyNumber(int arr[], int size)
-     {
-        
-         HashMap<Integer, Integer> freqMap = new HashMap<Integer, Integer>();
-
-         for (int i=0;i<size;i++)
-         {
-             if (freqMap.containsKey(arr[i]))
-             {
-                 freqMap.put(arr[i], freqMap.get(arr[i]) + 1);
-             }
-
-             else
-             {
-                 freqMap.put(arr[i], 1);
-             }
-         }
-
-         ArrayList<Integer> list = new ArrayList<Integer>();
-         LinkedHashMap<Integer, Integer> sortedMap = new LinkedHashMap<Integer, Integer>();
-
-         for (Map.Entry<Integer, Integer> entry : freqMap.entrySet())
-         {
-             list.add(entry.getValue());
-         }
-
-         Collections.sort(list, new Comparator<Integer>()
-         {
-             @Override
-             public int compare(Integer o1, Integer o2)
-             {
-                 if(o1>o2)
-                     return (1);
-                 else if(o2>o1)
-                     return(-1);
-                 else
-                     return (0);
-             }
-         });
-
-         for (int i : list)
-         {
-             for (Entry<Integer, Integer> entry1 : freqMap.entrySet())
-             {
-                 if (entry1.getValue().equals(i))
-                 {
-                     sortedMap.put(entry1.getKey(),i);
-                 }
-             }
-         }
-         System.out.println("\n"+"Number = Frequency");
-         System.out.println(sortedMap);
-     }
-
+ 
+class GFG
+{
+ 
+    static void countFreq(int arr[], int n)
+    {
+        Map<Integer, Integer> mp = new HashMap<>();
+ 
+        // Traverse through array elements and
+        // count frequencies
+        for (int i = 0; i < n; i++)
+        {
+            if (mp.containsKey(arr[i]))
+            {
+                mp.put(arr[i], mp.get(arr[i]) + 1);
+            }
+            else
+            {
+                mp.put(arr[i], 1);
+            }
+        }
+        // Traverse through map and print frequencies
+        for (Map.Entry<Integer, Integer> entry : mp.entrySet())
+        {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+ 
+    // Driver code
+    public static void main(String args[])
+    {
+        int arr[] = {10, 20, 20, 10, 10, 20, 5, 20};
+        int n = arr.length;
+        countFreq(arr, n);
+    }
 }
+ 
